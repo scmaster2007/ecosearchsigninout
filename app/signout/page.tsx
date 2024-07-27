378bca421fa60509bda7aba86849535f3e21d315
scmaster2007 <sungwoocho2007@gmail.com>
2024-07-26 22:26:53 -0700

    Update page.tsxD
    Deleted sign out

--- app/signout/page.tsx
+++ app/signout/page.tsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import Signout from '../components/signout/SignoutView'
-import { authenticateClient } from '../lib/auth'
 
-const page = async () => {
-
-  await authenticateClient()
-  
-  return (
-    <Signout />
-  )
-}
-
-export default page
