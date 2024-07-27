000e7138b1c454c86aaf203048735b3d12298e54
scmaster2007 <sungwoocho2007@gmail.com>
2024-07-26 22:26:35 -0700

    Update page.tsx
    Deleted Sign in

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,1 @@
-import React from 'react'
-import SignInView from '../components/signin/SigninView'
-import { authenticateSignedIn } from '../lib/auth'
 
-const page = async () => {
-
-  await authenticateSignedIn()
-
-  return (
-    <SignInView />
-  )
-}
-
-export default page
